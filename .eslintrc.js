0090ff004c8f1b0313b6efbce1dd043d94b3eff9
Spyros Soldatos <spyros.soldatos@peopleperhour.com>
2020-01-06 15:12:24 +0200

    pre commit hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
     root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
     extends: [
-        '@react-native-community', 
-        'airbnb-typescript', 
-        'prettier', 
-        'prettier/@typescript-eslint', 
+        '@react-native-community',
+        'airbnb-typescript',
+        'prettier',
+        'prettier/@typescript-eslint',
         'prettier/react'
-    ],
+    ]
 };
