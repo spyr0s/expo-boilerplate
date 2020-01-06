8e5763f6067c3c475761a0823648e7830f8ffd5a
Spyros Soldatos <spyros.soldatos@peopleperhour.com>
2020-01-06 14:21:59 +0200

    Add some useful dependancies

---
+++ src/app/redux/app/actions.ts
@@ -0,0 +1,16 @@
+import { AnyAction } from "redux";
+
+export const APP_STARTED = 'APP_STARTED';
+
+export interface AppAction extends AnyAction {
+    payload: {
+      appStarted: boolean;
+  
+    };
+  }
+
+export const appStarted = ()=>(dispatch) => {
+    return new Promise(resolve => {
+        resolve(dispatch({ type: APP_STARTED, payload: { appStarted: true } }));
+      });
+}
