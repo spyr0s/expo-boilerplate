dede2f993490f0fceea1f49bb0f4e6cd325b6350
Spyros Soldatos <spyros.soldatos@peopleperhour.com>
2020-04-22 17:21:31 +0300

    Upgrade SDK to 37

--- src/views/auth/AuthCheck.tsx
+++ src/views/auth/AuthCheck.tsx
@@ -43,11 +43,8 @@ class AuthCheck extends React.Component<Props, State> {
     }
 }
 
-const mapStateToProps = state => {
-    const { user } = state.auth;
-    return {
-        user
-    };
+const mapStateToProps = () => {
+    return {};
 };
 
 const mapDispatchToProps = () => {
